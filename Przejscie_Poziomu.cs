35fe21644fdb1325e87698cc9992869204c0922c
Input95 <78229391+Input95@users.noreply.github.com>
2021-06-29 13:55:12 +0200

    Add files via upload

---
+++ Przejscie_Poziomu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Przejscie_Poziomu : MonoBehaviour
+{
+    
+    public void LoadScene(string NazwaSceny)
+    {
+        SceneManager.LoadScene(NazwaSceny);
+    }
+
+
+ 
+}
